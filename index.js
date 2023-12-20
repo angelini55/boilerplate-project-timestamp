7c59c3cc93d57a5f1deadca69142a3327076a6cf
angelini55 <105080991+angelini55@users.noreply.github.com>
2023-12-20 20:55:30 +0000

    1st commit

--- index.js
+++ index.js
@@ -24,6 +24,34 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  let date = req.params.date;
+  if (date){
+    let dateObj = new Date(date);
+    if (dateObj.toString() === "Invalid Date"){
+      let unix = new Date(Number(date));
+      if (unix.toString() === "Invalid Date"){
+        res.json({error: "Invalid Date"})
+      } else{
+        res.json({ unix: Number(date), utc: unix.toUTCString() })
+      }
+    }else{
+      let unix = Math.floor(dateObj.getTime())
+      if (dateObj.toString() == "Invalid Date"){
+        res.json({ error : "Invalid Date" })
+        }else {
+          res.json({ unix: unix, utc: dateObj.toUTCString() })
+      }
+    }
+  }else {
+    let dateObj = new Date();
+    let unix = Math.floor(dateObj.getTime())
+      res.json({ unix: unix, utc: dateObj.toUTCString() })
+  }
+  
+  
+})
+
 
 
 // listen for requests :)
